47706c3b15c53cac8274a640b1cf62d231759bea
Christoph Schueler <cpu12.gems@googlemail.com>
2016-03-04 13:03:20 +0100

    setup.py: Added script 'msp430-info'. Renamed 'msp430-info.py' to 'msp430_info.py'.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author_email = 'cpu12.gems@googlemail.com',
     license = 'GPL',
     classifiers = [
-        'Development Status :: 4, // 3 - Alpha, 4 - Beta, 5- Production Stable',
+        'Development Status :: 3',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: GPL License',
@@ -54,9 +54,9 @@ setup(
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    #entry_points={
-    #    'console_scripts': [
-    #        'sample=sample:main',
-    #    ],
-    #},
+    entry_points={
+        'console_scripts': [
+            'msp430-info = msp430dll.msp430_info:main',
+        ],
+    },
 )
